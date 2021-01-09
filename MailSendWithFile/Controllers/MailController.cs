135ad8b10bf6b052a3a6a92c6585cd00cba78988
fundasarikaya <fundamelihcan@gmail.com>
2021-01-09 21:31:56 +0300

    first commit

---
+++ MailSendWithFile/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MailSendWithFile.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Mail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Mail(HttpPostedFileBase dosya, string Name, string LastFirm, string City, string Phone, string Email)
+        {
+            string path = Server.MapPath("~/Content" + dosya.FileName);
+            dosya.SaveAs(path);
+            SmtpClient mailClient = new SmtpClient("smtp.gmail.com", 587);
+            NetworkCredential cred = new NetworkCredential("@gmail.com", "sifre");
+            mailClient.Credentials = cred;
+            MailMessage contact = new MailMessage();
+            contact.From = new MailAddress("@gmail.com");
+            contact.Subject = "Dosya Yuklendi";
+            contact.IsBodyHtml = true;
+            contact.Body = " Ad : " + Name + " Son Çalışılan Firma : " + LastFirm + " Şehir : " + City + " Telefon : " + Phone + " Email : " + Email;
+            mailClient.EnableSsl = true;
+            Attachment a = new Attachment(dosya.InputStream, dosya.FileName);
+            contact.Attachments.Add(a);
+            contact.To.Add("@gmail.com");
+            mailClient.Send(contact);
+            return View();
+        }
+    }
+}
---
+++ MailSendWithFile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MailSendWithFile.MvcApplication" Language="C#" %>
